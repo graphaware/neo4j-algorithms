1193e21fed22acf7770f211055e0d9e38d58f213
Vojta Havlicek <vojta.havlicek@gmail.com>
2014-08-13 22:02:31 +0100

    Watts-Strogatz optimized

--- src/test/java/com/graphaware/module/algo/generator/relationship/WattsStrogatzRelationshipGeneratorTest.java
+++ src/test/java/com/graphaware/module/algo/generator/relationship/WattsStrogatzRelationshipGeneratorTest.java
@@ -1,18 +1,32 @@
 package com.graphaware.module.algo.generator.relationship;
 
 import com.graphaware.module.algo.generator.config.WattsStrogatzConfig;
-import com.graphaware.module.algo.generator.relationship.WattsStrogatzRelationshipGenerator;
 import junit.framework.TestCase;
-import org.junit.Ignore;
+import org.junit.Test;
 
-@Ignore
+import java.util.HashSet;
+
 public class WattsStrogatzRelationshipGeneratorTest extends TestCase {
 
-    public void testDoGenerateEdges() throws Exception {
-        WattsStrogatzRelationshipGenerator generator = new WattsStrogatzRelationshipGenerator(new WattsStrogatzConfig(100, 4, 0.5));
+    @Test
+    public void testDoGenerateEdgesValidity() throws Exception {
+        int meanDegree = 4;
+        int numberOfNodes = 10;
+        double betaCoefficient = 0.5;
 
-        for (int i = 0; i < 10; ++i)
-            System.out.println(generator.generateEdges().toString());
-            System.out.println("-------------------------------------");
+        WattsStrogatzRelationshipGenerator generator = new WattsStrogatzRelationshipGenerator(new WattsStrogatzConfig(numberOfNodes, meanDegree, betaCoefficient));
+
+        System.out.println(generator.doGenerateEdges());
+        assertEquals((int) (meanDegree*numberOfNodes*.5), generator.doGenerateEdges().size());
+    }
+
+    @Test(timeout = 2 * 60 * 1000)
+    public void testDoGenerateEdgesPerformance() throws Exception {
+        int meanDegree = 4;
+        int numberOfNodes = 2_000_000;
+        double betaCoefficient = 0.5;
+
+        WattsStrogatzRelationshipGenerator generator = new WattsStrogatzRelationshipGenerator(new WattsStrogatzConfig(numberOfNodes, meanDegree, betaCoefficient));
+        assertEquals((int) (meanDegree*numberOfNodes*.5), generator.doGenerateEdges().size());
     }
 }
